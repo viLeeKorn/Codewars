2d0930fc6e67d09c240a41e9b329647fc5f7abb4
Victor Kohut <victor1kohut@gmail.com>
2022-02-08 21:56:49 +0300

    Add Grasshoper - Messi Goals.swift

---
+++ Swift/8-kuy/Grasshoper - Messi Goals.swift
@@ -0,0 +1,23 @@
+/*
+Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Task
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+*/
+var laLigaGoals:Int = 43
+var championLeagueGoals:Int = 10
+var copaDelReyGoals:Int = 5
+
+var totalGoals:Int = laLigaGoals + championLeagueGoals + copaDelReyGoals
