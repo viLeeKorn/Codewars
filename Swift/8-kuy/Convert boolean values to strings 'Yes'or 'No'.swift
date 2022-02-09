90d01ef24129d59dbdbc7c57461c78cd87d7e9df
Victor Kohut <victor1kohut@gmail.com>
2022-02-09 08:00:42 +0300

    Add two for swift

---
+++ Swift/8-kuy/Convert boolean values to strings 'Yes'or 'No'.swift
@@ -0,0 +1,6 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+func boolToWord(_ bool: Bool) -> String {
+  return bool ? "Yes" : "No"
+}
