8ed76d74cbaa83e7f8a5719d416a29c5866f12b2
Victor Kohut <victor1kohut@gmail.com>
2022-02-14 23:39:22 +0300

    Add Calculate BMI.swift

---
+++ Swift/8-kuy/Calculate BMI.swift
@@ -0,0 +1,15 @@
+/*
+Write function bmi that calculates body mass index (bmi = weight / height2).
+if bmi <= 18.5 return "Underweight"
+if bmi <= 25.0 return "Normal"
+if bmi <= 30.0 return "Overweight"
+if bmi > 30 return "Obese"
+*/
+func bmi(_ weight: Int, _ height: Double) -> String {
+    switch Double(weight)/pow(height, 2) {
+      case ...18.5: return "Underweight"
+      case ...25.0: return "Normal"
+      case ...30.0: return "Overweight"
+      default: return "Obese"
+    }
+}
